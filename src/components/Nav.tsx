e7aaca95d0fa216d3bba1d49a624eeac4ad7edf8
scottmsaller <scottmsaller@gmail.com>
2024-10-25 19:04:15 -0400

    added navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,10 @@
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav className="nav d-start">
+        <p className="nav-item"><a className="nav-link" href="/">Candidate Search</a></p>
+        <p className="nav-item"><a className="nav-link" href="/SavedCandidates">Saved Candidates</a></p>
+    </nav>
   )
 };
 
