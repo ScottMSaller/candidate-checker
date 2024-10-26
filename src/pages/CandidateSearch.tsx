d8550b248d236325aa17cc04d601c6d0ab1d8837
scottmsaller <scottmsaller@gmail.com>
2024-10-25 22:02:46 -0400

    began work on candidateSearch.tsx and started to implement functions to destructure the info recieved from the api calls

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -2,7 +2,37 @@ import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [users, setUsers] = useState([]);
+  const [users2, setUsers2] = useState([]);
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const data = await searchGithub();
+        setUsers(data);
+      }
+      catch(error) {
+        console.log(error);
+      } 
+    }
+    fetchData();
+  }, []);
+  
+  useEffect(() => {
+    const fetchData = async() => {
+      users.map(async(user: any)=> {
+      const data = await searchGithubUser(user.login)
+      console.log(user.login)
+      setUsers2(data);
+      })
+    }
+    fetchData();
+    console.log(users2)
+  },[])
+
+  
+  return <div>
+  <h1>CandidateSearch</h1>
+  </div>;
 };
 
 export default CandidateSearch;
