961853a38ccdf448828c2f02eddb55c39f4eb3b3
Scott Saller <scottmsaller@gmail.com>
2024-11-01 10:52:53 -0400

    got home page of project to properly work

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+interface CandidateI {
+    avatar_url: string;
+    login: string;
+    location: string;
+    email: string;
+    company: string;
+    bio: string;
+}
+
+export default CandidateI;
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,61 @@
 import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
-
+import { searchGithub, searchGithubUser } from '../api/api';
+import CandidateI from '../interfaces/Candidate.interface';
+import CandidateCard from '../components/CandidateCard';
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [userList, setList] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [currentUser, setUser] = useState<CandidateI | null>(null);
+  useEffect(() => {
+    const fetchUserList = async() => {
+    const data  = await searchGithub();
+    const tempData = data.map((user: any)=> {
+      return user.login;
+    });
+    setList(tempData);
+    };
+    fetchUserList();
+  },[]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const data = await searchGithubUser(userList[index]);
+      const keys = ['avatar_url', 'login', 'location', 'email', 'company', 'bio'];
+      
+      // Filter the data object to only include the specified keys
+      const filteredData = keys.reduce((obj: { [key: string]: any }, key) => {
+        if (data.hasOwnProperty(key)) {
+          obj[key] = data[key];
+        }
+        return obj;
+      }, {});
+  
+      console.log(filteredData); // This will log the filtered object
+      setUser(filteredData as CandidateI)
+    };
+  
+    fetchUser();
+  }, [userList, index]); // Add dependencies if needed
+
+  return (
+    <div>
+    <h1>CandidateSearch</h1>
+    <CandidateCard 
+      avatar_url={currentUser?.avatar_url || ''} 
+      login={currentUser?.login || ''} 
+      location={currentUser?.location || ''} 
+      email={currentUser?.email || ''} 
+      company={currentUser?.company || ''} 
+      bio={currentUser?.bio || ''} 
+    />
+    <button onClick={() => setIndex(index + 1)}>No</button>
+    <button onClick={() => { 
+      setIndex(index + 1); 
+      const currentArray = JSON.parse(localStorage.getItem('userArray') || '[]');
+      currentArray.push(currentUser);
+      localStorage.setItem('userArray', JSON.stringify(currentArray)); }}>Yes</button>
+    </div>
+  )
 };
 
 export default CandidateSearch;
